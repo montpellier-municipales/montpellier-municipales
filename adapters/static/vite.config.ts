41b7726c17d63b2e354d27c9c0eaa092f276f6a6
Arthur Lenoir <me@arthurlenoir.fr>
2025-12-14 18:59:51 +0100

    chore: add deployment to github pages + set domain name

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "http://localhost:8080",
+        origin: "https://montpellier-municipales.fr",
       }),
     ],
   };
